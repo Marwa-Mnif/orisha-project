9708a4eec8cec1fea40369f8b21552948c281a5d
Marwa Mnif <marwa.mnif@hotmail.com>
2025-10-23 00:11:53 +0200

    [Marwa] chore:Prepare projet components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,27 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Sidebar from "../components/Sidebar";
+import Search from "../components/Search";
+import Paginate from "../components/Paginate";
+import PokemonList from "../components/PokemonList";
+import React from "react";
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+
+      <main className="flex flex-1 p-4 gap-4">
+        <aside className="w-1/4">
+          <Sidebar />
+        </aside>
+        <section className="flex-1 flex flex-col gap-4">
+          <Search />
+          <PokemonList />
+          <Paginate />
+        </section>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
