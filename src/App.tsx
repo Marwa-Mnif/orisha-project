29fdd20536992d5da7d9c255bcac716e6b3f50a4
Marwa Mnif <marwa.mnif@hotmail.com>
2025-10-22 23:37:44 +0200

    [Marwa] chore:Initialize projet and install dependencies

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="tw-bg-blue-500 tw-text-white tw-p-4 tw-rounded">
+      React + Tailwind fonctionne !
+    </div>
+  );
+}
