26824f5829af7c3489b2f62fb4f53150985897ba
Marwa Mnif <marwa.mnif@hotmail.com>
2025-10-26 13:40:02 +0100

    [Marwa] feat: add loading to hook

--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -4,15 +4,18 @@ import { fetchAllCharacters } from '../services/rickApi';
 
 export function useCharacters(search: string, page: number) {
   const [data, setData] = useState<CharactersResponse | null>(null);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
+    setLoading(true);
     setError(null);
 
     fetchAllCharacters({ name: search, page })
       .then(setData)
       .catch(err => setError(err.message))
+      .finally(() => setLoading(false));
   }, [search, page]);
 
-  return { data, error };
+  return { data, loading, error };
 }
