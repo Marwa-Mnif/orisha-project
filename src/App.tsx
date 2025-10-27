3352452a7d40a127bd8196eb18623ab71d44c2e8
Marwa Mnif <marwa.mnif@hotmail.com>
2025-10-28 00:39:01 +0100

    [Marwa] Chore: clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,3 @@
-import React from "react";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-
 export default function App() {
-  return (
-    <>
-      <Header />
-      <div className="tw-bg-blue-500 tw-text-white tw-p-4 tw-rounded">
-        React + Tailwind fonctionne !
-      </div>
-      <Footer />
-    </>
-  );
+  return;
 }
